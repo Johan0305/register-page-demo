62427b45539114a529beeef95eb60ce95d6fbcc9
Johan Sora <johansp0305@gmail.com>
2022-08-23 17:35:56 -0500

    add registration page for user and admin

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../components/Landing/Footer";
+import Header from "../components/Landing/Header";
+import Main from "../components/Landing/Main";
+
+const Landing = () => {
+  return (
+    <div id="container-global-landing">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+};
+
+export default Landing;
